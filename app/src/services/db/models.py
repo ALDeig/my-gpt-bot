1df5f1965215abb79b2045ea4d50a3f43b47712d
renovatio <alkin.denis@gmail.com>
2023-12-13 10:53:12 +0400

    refactor: обновил модели БД

--- app/src/services/db/models.py
+++ app/src/services/db/models.py
@@ -1,7 +1,8 @@
 import enum
+from typing import Optional
 
 from sqlalchemy import BigInteger, ForeignKey, Integer, String, Enum
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.src.services.db.base import Base
 
@@ -58,3 +59,55 @@ class Settings(Base):
     image_format: Mapped[ImageForamt] = mapped_column(
         Enum(ImageForamt), default=ImageForamt.SQUARE
     )
+
+
+class Settings2(Base):
+    __tablename__ = "settings"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey(User.id, ondelete="CASCADE"))
+    gpt_model_id: Mapped[int] = mapped_column(ForeignKey("gpt_models.id"))
+    gpt_model: Mapped["GPTModel"] = relationship()
+    tts_voice_id: Mapped[Optional[int]] = mapped_column(ForeignKey("voices.id"))
+    tts_voice: Mapped[Optional["Voice"]] = relationship()
+    image_style_id: Mapped[Optional[int]] = mapped_column(ForeignKey("image_styles.id"))
+    image_style: Mapped[Optional["ImageStyle"]] = relationship()
+    image_format_id: Mapped[Optional[int]] = mapped_column(ForeignKey("image_format.id"))
+    image_format: Mapped[Optional["ImageForamt"]] = relationship()
+
+
+class Voice(Base):
+    __tablename__ = "voices"
+    
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    verbose_name: Mapped[str]
+    desctiption: Mapped[str] = mapped_column(String, nullable=True)
+
+
+class ImageStyle(Base):
+    __tablename__ = "image_styles"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    verbose_name: Mapped[str]
+    desctiption: Mapped[str] = mapped_column(String, nullable=True)
+
+
+class ImageFormat(Base):
+    __tablename__ = "image_formats"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    verbose_name: Mapped[str]
+    desctiption: Mapped[str] = mapped_column(String, nullable=True)
+
+
+class GPTModel(Base):
+    __tablename__ = "gpt_models"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    verbose_name: Mapped[str]
+    desctiption: Mapped[str] = mapped_column(String, nullable=True)
+
