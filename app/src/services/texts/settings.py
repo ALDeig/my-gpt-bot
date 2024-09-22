c69d58398cdf5287707369b23e958693d83acfaf
aldeig <alkin.denis@gmail.com>
2024-09-22 17:37:14 +0400

    refactor: moved texts to modules

---
+++ app/src/services/texts/settings.py
@@ -0,0 +1,17 @@
+from app.src.services.db.models import Settings, TTSVoice
+
+SELECT_OPTIONS = "Выберите один из вариантов"
+
+
+def settings_text(settings: Settings) -> str:
+    tts_voice_text = (
+        "Не выбран"
+        if settings.tts_voice == TTSVoice.NOT_SELECT
+        else settings.tts_voice.value
+    )
+    return (
+        f"🆔 Ваш id: {settings.user_id}\n"
+        f"🔊 Голос: {tts_voice_text}\n"
+        f"🎨 Стиль: {settings.image_style.value}\n"
+        f"📐 Формат: {settings.image_format.value}"
+    )
